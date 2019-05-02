360de20123409b63056072a9966a8f4575427d54
Alex <alexanderdfree@gmail.com>
2019-05-01 17:20:27 -0700

    pasted encryptor into decryptor and am working on it

--- Decryptor.java
+++ Decryptor.java
@@ -1,33 +1,53 @@
 
 /**
- * Write a description of class Decryptor here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Alex
+ * @version -1
  */
+import java.util.Scanner;
+
 public class Decryptor
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Decryptor
-     */
     public Decryptor()
     {
-        // initialise instance variables
-        x = 0;
+        Scanner scanXD = new Scanner(System.in);
+        System.out.print("Which message would you like to decrypt? ");
+        String ciphertext = scanXD.nextLine();
+        System.out.print("What is the key for the cipher? ");
+        Scanner scanXD2 = new Scanner(System.in);
+        char z;
+        String plaintext = "";
+        int x = scanXD2.nextInt();
+        String gamerz = "";
+        for (int riseUp = 0; riseUp < ciphertext.length(); riseUp++) {
+            char xd = ciphertext.charAt(riseUp);
+            ciphertext = ""; //initializes cipher
+            if (xd <= 'z' && xd >= 'a') {
+                //int z = (int)xd-(int)'a'+ 1;
+                //char nowThatsHowGamersDoIt = (char)z;
+                z = (char)(xd+x);
+                if (z > 'Z') {
+                    z = (char)(z - 1 +'a' + 'z');
+                }
+                plaintext += z;//nowThatsHowGamersDoIt;
+            }
+            if (xd <= 'Z' && xd >= 'A'){
+                //int gamersRiseUpInTheClub = (int)xd-(int)'A'+ 1;
+                //char gamer = (char)gamersRiseUpInTheClub;
+                z = (char)(xd+x);
+                if (z > 'Z') {
+                    z = (char)(z - 1 +'A' + 'Z');
+                }
+                plaintext += z; //gamer;
+            }
+            else {
+               plaintext += xd; 
+            }
+            gamerz = plaintext;
+        }
+        //please work
+        System.out.println("Your encrypted text is: " + gamerz + ".");
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
+
