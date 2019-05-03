ae24ed438e8f8b5b24909a7e4e69d5a128f80d2e
Alex <alexanderdfree@gmail.com>
2019-05-03 09:26:22 -0700

    made rsa

---
+++ RSAEncrypt.java
@@ -0,0 +1,33 @@
+import java.util.Random;
+/**
+ * Write a description of class RSAEncrypt here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RSAEncrypt
+{
+    private int x;
+    public int randomNumberGen(){
+        int number = 0;
+        Random randomNumber = new Random(); // generate a random number
+        number = randomNumber.nextInt(500);
+        /*while (number > 0) {
+        if (isPrime(number) != true) {
+            number--;
+        }
+        else{
+            return number;
+            break;
+        }*/
+        return 0;
+    }}
+ 
+    /*String RSAEncrypt()
+    {
+        // initialise instance variables
+        
+        x = 0;
+    }*/
+ 
+//}
---
+++ primeCheck.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class isPrime here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class primeCheck
+{
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        //return x + y;
+        return 0;
+    }
+}
