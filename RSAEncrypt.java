2d70d8e24f4491a03596c3822685f48dbe4ece5d
Alex <alexanderdfree@gmail.com>
2019-05-03 15:04:05 -0700

    attempted to fix rsaencrypt

--- RSAEncrypt.java
+++ RSAEncrypt.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
+//import java.util.Math;
 /**
  * Write a description of class RSAEncrypt here.
  *
@@ -45,32 +46,41 @@ double lcm(double a, double b)
     
 }
 public boolean coPrime(double x, double y) {
-    if (gcf(x,y) == 0) {
+    if (gcf(x,y) == 0 || gcf(x,y) == 1) {
         return true;
     }
     else{return false;}
 }
-    public RSAEncrypt() {
-        Scanner scanXD = new Scanner(System.in);
+    public void RSAEncrypt() {
         System.out.print("Which message would you like to encrypt? ");
-        String plaintext = scanXD.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String plaintext = scanner.nextLine();
         String codetext = "";
-        double p = getRandomInt(1,5000);
-        double q = getRandomInt(1,5000);
+        double codeTemp = 0;
+        double p = getRandomInt(1,500);
+        double q = getRandomInt(1,500);
         while (primeCheck(p) == false || primeCheck(q) == false) {
-            p = getRandomInt(1,5000);
-            q = getRandomInt(1,5000);
+            p = getRandomInt(1,500);
+            q = getRandomInt(1,500);
         }
         double n = p * q;
         double lambda = lcm(p-1,q-1);
         System.out.println("Your \"n\" is " + n + ".");
         System.out.println("Your \"lambda\" is " + lambda + ". Keep this secret.");
         double e = getRandomInt(2,lambda-1);
-        while (coPrime(e,lambda) != true)
+        char furtherTemp;
+        while (coPrime(e,lambda) == false)
         {
             e = getRandomInt(2,lambda-1);
         }
         System.out.println("Your \"e\" is " + e + ".");
+        for (int riseUp = 0; riseUp < plaintext.length(); riseUp++) {
+            char currentChar = plaintext.charAt(riseUp);
+            codeTemp =  (Math.pow((int)currentChar,e))%n;
+            furtherTemp = (char)((Math.pow((int)currentChar,e))%n);
+            System.out.println(codeTemp);
+            codetext += codeTemp;
+        }
         System.out.println(codetext);
     }}
 
